b99d1e149cc3423914e59ec97c9ed741a85ccc26
SgermosenW05 <Sgermosen@W05>
2017-05-10 15:13:47 -0400

    First Upload of the project

---
+++ DenunciadoWeb/DenunciadoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DenunciadoWeb.Startup))]
+namespace DenunciadoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
