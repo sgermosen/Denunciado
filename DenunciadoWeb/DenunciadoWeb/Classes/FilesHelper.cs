b27ca110da5a85dee5200b7c91469e2d36074760
SgermosenW05 <Sgermosen@W05>
2017-05-29 12:15:36 -0400

    Creating the Helper and ineheresing from the main tables

---
+++ DenunciadoWeb/DenunciadoWeb/Classes/FilesHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace DenunciadoBackEnd.Classes
+{
+    public class FilesHelper
+    {
+        public static string UploadPhoto(HttpPostedFileBase file, string folder)
+        {
+            var path = string.Empty;
+            var pic = string.Empty;
+
+            if (file != null)
+            {
+                pic = Path.GetFileName(file.FileName);
+                path = Path.Combine(HttpContext.Current.Server.MapPath(folder), pic);
+                file.SaveAs(path);
+                //using (MemoryStream ms = new MemoryStream())
+                //{
+                //    file.InputStream.CopyTo(ms);
+                //    byte[] array = ms.GetBuffer();
+                //}
+            }
+
+            return pic;
+        }
+    }
+}
--- DenunciadoWeb/DenunciadoWeb/Models/ComplaintView.cs
+++ DenunciadoWeb/DenunciadoWeb/Models/ComplaintView.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Web;
 
 namespace DenunciadoBackEnd.Models
 {
-    public class ComplaintView
+    [NotMapped]
+    public class ComplaintView: Complaint
     {
         [Display(Name = "Image")]
         public HttpPostedFileBase ImageFile { get; set; }
