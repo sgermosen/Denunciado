4204c2fa6e5cd7b1c51128fd196264cf1e086299
SgermosenW05 <Sgermosen@W05>
2017-05-29 12:30:44 -0400

    Create View for Complaints

---
+++ DenunciadoWeb/DenunciadoWeb/Controllers/ComplaintsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DenunciadoBackEnd.Models;
+
+namespace DenunciadoBackEnd.Controllers
+{
+    public class ComplaintsController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Complaints
+        public ActionResult Index()
+        {
+            //return View(db.Complaints.OrderBy(f => f.Description).ToList());
+            var complaints = db.Complaints.Include(c => c.ComplaintType);
+            return View(complaints.ToList());
+        }
+
+        // GET: Complaints/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Complaint complaint = db.Complaints.Find(id);
+            if (complaint == null)
+            {
+                return HttpNotFound();
+            }
+            return View(complaint);
+        }
+
+        // GET: Complaints/Create
+        public ActionResult Create()
+        {
+            ViewBag.ComplaintTypeId = new SelectList(db.ComplaintTypes, "ComplaintTypeId", "Code");
+            return View();
+        }
+
+        // POST: Complaints/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ComplaintId,UserId,CreationDate,Address,Description,Image,IsActive,Lat,Lon,ComplaintTypeId")] Complaint complaint)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Complaints.Add(complaint);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ComplaintTypeId = new SelectList(db.ComplaintTypes, "ComplaintTypeId", "Code", complaint.ComplaintTypeId);
+            return View(complaint);
+        }
+
+        // GET: Complaints/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Complaint complaint = db.Complaints.Find(id);
+            if (complaint == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ComplaintTypeId = new SelectList(db.ComplaintTypes, "ComplaintTypeId", "Code", complaint.ComplaintTypeId);
+            return View(complaint);
+        }
+
+        // POST: Complaints/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ComplaintId,UserId,CreationDate,Address,Description,Image,IsActive,Lat,Lon,ComplaintTypeId")] Complaint complaint)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(complaint).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ComplaintTypeId = new SelectList(db.ComplaintTypes, "ComplaintTypeId", "Code", complaint.ComplaintTypeId);
+            return View(complaint);
+        }
+
+        // GET: Complaints/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Complaint complaint = db.Complaints.Find(id);
+            if (complaint == null)
+            {
+                return HttpNotFound();
+            }
+            return View(complaint);
+        }
+
+        // POST: Complaints/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Complaint complaint = db.Complaints.Find(id);
+            db.Complaints.Remove(complaint);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
