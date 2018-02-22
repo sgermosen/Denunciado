b79962c521f54a945a6f1ccf4824e7e2d8440e2a
SgermosenW05 <Sgermosen@W05>
2018-02-22 17:12:35 -0400

    Incluyendo la tabla de estatus

---
+++ DenunciadoWeb/Domain/Status.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Status
+    {
+        [Key]
+        public int StatusId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MaxLength(50, ErrorMessage = "The maximun length for field {0} is {1} characters")]
+        [Index("Status_Name_Index", IsUnique = true)]
+        [Display(Name = "Status")]
+        public string Name { get; set; }
+
+       // public virtual ICollection<Match> Matches { get; set; }
+    }
+
+}
