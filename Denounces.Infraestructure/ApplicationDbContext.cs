2d5d24499f557513099ebd76f6854949e28cdc5c
sGermosenMac <sgermosen@praysoft.net>
2020-08-23 19:49:36 -0400

    seeding

--- Denounces.Infraestructure/ApplicationDbContext.cs
+++ Denounces.Infraestructure/ApplicationDbContext.cs
@@ -5,9 +5,11 @@
     using Domain.Entities;
     using EntityConfigurations;
     using Infraestructure.Extensions;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
@@ -28,6 +30,9 @@
 
             foreach (var fk in cascadeFKs)
                 fk.DeleteBehavior = DeleteBehavior.Restrict;
+
+            //seeding initial data to the database
+            SeedData(modelBuilder);
 
             base.OnModelCreating(modelBuilder);
 
@@ -55,6 +60,64 @@
         public DbSet<Province> Provinces { get; set; }
         public DbSet<Status> Status { get; set; }
         public DbSet<Vote> Votes { get; set; }
+
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+
+            var rolAdminId = "9aae0b6d-d50c-4d0a-9b90-2a6873e3845d";
+            var usuarioAdminId = "5673b8cf-12de-44f6-92ad-fae4a77932ad";
+
+            var rolAdmin = new IdentityRole()
+            {
+                Id = rolAdminId,
+                Name = "Admin",
+                NormalizedName = "Admin"
+            };
+
+            var passwordHasher = new PasswordHasher<IdentityUser>();
+
+            var username = "sgrysoft@gmail.com";
+
+            var usuarioAdmin = new IdentityUser()
+            {
+                Id = usuarioAdminId,
+                UserName = username,
+                NormalizedUserName = username,
+                Email = username,
+                NormalizedEmail = username,
+                PasswordHash = passwordHasher.HashPassword(null, "Aa123456*")
+            };
+
+            modelBuilder.Entity<ProposalType>()
+               .HasData(new List<ProposalType>
+               {
+                    new ProposalType{Id = 1, Description= "Proposal", Name= "Proposal" },
+                    new ProposalType{Id = 2, Description= "Denounce", Name= "Denounce" },
+                       });
+
+            modelBuilder.Entity<Country>()
+              .HasData(new List<Country>
+              {
+                    new Country{Id = 1,  Denomym= "Dominicans", Name= "Dominican Republic" },
+                    new Country{Id = 2,  Denomym= "Haitian", Name= "Haiti" },
+                      });
+
+            modelBuilder.Entity<Province>()
+             .HasData(new List<Province>
+             {
+                    new Province{Id = 1,  CountryId = 1, Name= "Santo Domingo" , Initials = "SD"},
+                     });
+
+            modelBuilder.Entity<Status>()
+              .HasData(new List<Status>
+              {
+                    new Status{Id = 1, Table= "Proposal", Name= "Active" },
+                    new Status{Id = 2, Table= "Proposal", Name= "Closed" },
+                    new Status{Id = 3, Table= "Proposal", Name= "Rejected" },
+                    new Status{Id = 4, Table= "Proposal", Name= "Promoted" },
+                    new Status{Id = 5, Table= "Proposal", Name= "Applied" }, 
+                      });            
+        }
 
         public override int SaveChanges()
         {
